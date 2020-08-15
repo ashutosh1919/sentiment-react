93f99baf15c3c2bb2045cf261c311ca1fa1b01c0
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-08-15 13:13:21 +0530

    Updates

---
+++ precache-manifest.748a1ad043c9b4b3eb54318e66ae31a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99673f4f846763adf1c706ca89748ea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b3a4f9d5f4478f3c47e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d55cd4dd728860b6668",
+    "url": "/static/js/2.cf9a94ac.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.cf9a94ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3a4f9d5f4478f3c47e",
+    "url": "/static/js/main.51ae1c39.chunk.js"
+  },
+  {
+    "revision": "e44ab6c9992206d9bdeb",
+    "url": "/static/js/runtime-main.58e7c82b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
